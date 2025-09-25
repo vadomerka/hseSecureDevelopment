dafd1c50b25d3ec5d5d37fdab873fac13a9e7040
vadomerka <pradomiri@gmail.com>
2025-09-25 22:52:19 +0300

    Task Tracker base API

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
 
-app = FastAPI(title="SecDev Course App", version="0.1.0")
+app = FastAPI(title="Task Tracker", version="0.1.0")
 
 
 class ApiError(Exception):
@@ -29,29 +29,79 @@ async def http_exception_handler(request: Request, exc: HTTPException):
     )
 
 
-@app.get("/health")
-def health():
-    return {"status": "ok"}
-
-
 # Example minimal entity (for tests/demo)
-_DB = {"items": []}
+"""
+- id
+- title
+- description
+- type
+- status
+- priority
+- tag
+- due_at
+- started_at
+"""
+_DB = {
+    "Users": [
+        {"id": 1, "name": "", "email": "", "password": ""},
+        {"id": 2, "name": "", "email": "", "password": ""},
+    ],
+    "Tasks": [
+        {
+            "id": 1,
+            "title": "Task 1",
+            "description": "",
+            "type": "Task",
+            "status": "",
+            "priority": 0,
+            "tag": "",
+            "due_at": "",
+            "started_at": "",
+        },
+        {
+            "id": 2,
+            "title": "Task 1",
+            "description": "",
+            "type": "Task",
+            "status": "",
+            "priority": 0,
+            "tag": "",
+            "due_at": "",
+            "started_at": "",
+        },
+    ],
+}
 
 
-@app.post("/items")
-def create_item(name: str):
-    if not name or len(name) > 100:
-        raise ApiError(
-            code="validation_error", message="name must be 1..100 chars", status=422
-        )
-    item = {"id": len(_DB["items"]) + 1, "name": name}
-    _DB["items"].append(item)
-    return item
+@app.get("/users")
+def get_users():
+    # Admin view
+    return _DB["users"]
 
 
-@app.get("/items/{item_id}")
-def get_item(item_id: int):
-    for it in _DB["items"]:
-        if it["id"] == item_id:
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    # Profile view
+    for it in _DB["users"]:
+        if it["id"] == user_id:
             return it
     raise ApiError(code="not_found", message="item not found", status=404)
+
+
+# @app.post("/users")
+# def authorize(name: str):
+#     if not name or len(name) > 100:
+#         raise ApiError(
+#             code="validation_error", message="name must be 1..100 chars", status=422
+#         )
+#     item = {"id": len(_DB["items"]) + 1, "name": name}
+#     _DB["items"].append(item)
+#     return item
+#
+#
+# @app.get("/items/{item_id}")
+# def get_item(item_id: int):
+#     for it in _DB["items"]:
+#         if it["id"] == item_id:
+#             return it
+#     raise ApiError(code="not_found", message="item not found", status=404)
